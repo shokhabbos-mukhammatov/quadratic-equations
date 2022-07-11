7d979b27400940dc378d3ddbbb3a1d32aab97a65
shokhabbos-mukhammatov <86566282+shokhabbos-mukhammatov@users.noreply.github.com>
2022-07-11 21:53:03 +0500

    Update Main.java

--- quadratic equations/src/Main.java
+++ quadratic equations/src/Main.java
@@ -3,10 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 GivenNumbers givnum = new GivenNumbers();
-givnum.A=-1;
-givnum.B=1;
+givnum.A=1;
+givnum.B=3;
 givnum.C=-3;
-givnum.solution(givnum.A, givnum.B, givnum.C);
+System.out.println(givnum);
+System.out.println(givnum.solution());
 
 	}
 
